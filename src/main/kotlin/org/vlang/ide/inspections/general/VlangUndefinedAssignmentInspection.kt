05b43f2158a23829fefe497737a39f7d702315d1
IkeVoodoo <simone.coletti07@gmail.com>
2022-10-21 12:11:27 +0200

    Finish implementing #178, continue working on #5 and fixes warning

--- src/main/kotlin/org/vlang/ide/inspections/general/VlangUndefinedAssignmentInspection.kt
+++ src/main/kotlin/org/vlang/ide/inspections/general/VlangUndefinedAssignmentInspection.kt
@@ -1,7 +1,10 @@
 package org.vlang.ide.inspections.general
 
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElementVisitor
 import org.vlang.ide.inspections.VlangBaseInspection
 import org.vlang.lang.psi.VlangAssignmentStatement
@@ -13,13 +16,30 @@ class VlangUndefinedAssignmentInspection : VlangBaseInspection() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return object : VlangVisitor() {
             override fun visitAssignmentStatement(o: VlangAssignmentStatement) {
-                holder.registerProblem(
-                    o.assignOp,
-                    "Testing",
-                    ProblemHighlightType.ERROR
-                )
+                val left = o.leftHandExprList
+                val last = left.lastChild
+                val ref = last.reference?.resolve()
+                if(ref == null) {
+                    holder.registerProblem(
+                        last,
+                        "Assignment to non-existent variable '$last'",
+                        ProblemHighlightType.ERROR, DEFINITION_FIX
+                    )
+                }
             }
         }
     }
 
+    class DefinitionQuickFix : LocalQuickFix {
+        override fun getFamilyName() = "Convert to definition"
+
+        override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+            // TODO convert = to :=
+        }
+
+    }
+
+    companion object {
+        private val DEFINITION_FIX = DefinitionQuickFix()
+    }
 }
